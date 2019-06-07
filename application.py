f3658924bec2003759962631e7d3ca6255a6300c
guoshen <guo.shen@husky.neu.edu>
2019-06-07 10:09:20 -0700

    show categories

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Item, Category, User
@@ -12,11 +12,33 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Read
+# Show the categories and the latest added items
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+@app.route('/category')
+def show_categories_and_latest_items():
+    categories = session.query(Category).all()
+    items = session.query(Item).order_by(Item.item_id.desc())
+    return render_template('categories.html', categories = categories)
+
+# Show one category and the items in it
+@app.route('/category/<int:category_id>')
+@app.route('/category/<int:category_id>/items')
+def show_one_category_and_items(category_id):
+    #categories = session.query(Category).all()
+    category = session.query(Category).filter_by(category_id=Category.category_id).first()
+    category_name = category.name
+    category_items = session.query(Item).filter_by(category_id = Item.category_id).all()
+    return render_template('category.html', category_items = category_items, category_name = category_name)
+
+
+
+'''
+# Create an item
+@app.route('/create')
+def create_item():
+    session.add(new_item)
+    session.commit()
+  '''  
 
 if __name__ == '__main__':
     app.debug = True
